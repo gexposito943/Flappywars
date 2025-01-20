e3f2fd5f8b9d35845290d98357ab7466cf4fefba
gexposito943 <gexposito943@boscdelacoma.cat>
2025-01-20 19:21:57 +0100

    CREATE TABLE: naus

--- database/taules.sql
+++ database/taules.sql
@@ -1,3 +1,15 @@
 -- Crear la base de dades
-CREATE DATABASE flappy_wars_db;
-USE flappy_wars_db;
+CREATE DATABASE flappywars_db;
+USE flappywars_db;
+
+--CRUD SIMPLE (taula sense claus foraneas)
+--taula naus espaciales
+CREATE TABLE naus (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL,
+    velocitat INT NOT NULL DEFAULT 1,
+    imatge_url VARCHAR(255),
+    descripcio TEXT,
+    disponible BOOLEAN DEFAULT true,
+    data_creacio TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
