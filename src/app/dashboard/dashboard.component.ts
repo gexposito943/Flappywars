829af62d464fff2097bf1db1dbe6ad1e37bacac4
gexposito943 <gexposito943@boscdelacoma.cat>
2025-01-21 16:17:47 +0100

    IMPLEMENTING TEST: Canvas element

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [CommonModule],
-  template: `<div></div>`,
+  template: `
+    <canvas #gameCanvas 
+            [width]="canvasWidth" 
+            [height]="canvasHeight">
+    </canvas>
+  `,
   styles: []
 })
 export class DashboardComponent {
+  @ViewChild('gameCanvas', { static: true }) gameCanvas!: ElementRef<HTMLCanvasElement>;
   score: number = 0;
   isGameRunning: boolean = false;
   canvasWidth: number = 800;
