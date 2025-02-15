0778938d8c9e8c0f335f850456ff09d533d4e978
Aviflyapp <aviflyapp@gmail.com>
2025-02-15 11:39:24 +0100

    IMPLEMENTING TEST:should redirect to home if no user data

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { RegistreService } from '../../services/registre.service';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './perfil.component.html',
-  styleUrl: './perfil.component.css'
+  styleUrls: ['./perfil.component.css']
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
+  constructor(
+    private registreService: RegistreService,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    const userData = this.registreService.getUserData();
+    if (!userData) {
+      this.router.navigate(['/']);
+    }
+  }
 }
